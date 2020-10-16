9e56987ba7113946c016b6e3c0b757ada954ba84
kamruzzamanK <officialkamran48@gmail.com>
2020-10-16 22:20:18 +0600

    Update status done

--- src/components/Dashboard/AdminDash/ServiceList/ServiceList.js
+++ src/components/Dashboard/AdminDash/ServiceList/ServiceList.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../Sidebar/Sidebar";
 import "./ServiceList.css";
 
 const ServiceList = () => {
-  const handleStatusChange = () => {
+  const [selectedOption, setSelectedOption] = useState({});
+
+  const handleStatusChange = (e) => {
     var x = document.getElementById("status").selectedIndex;
     var color = document.getElementsByTagName("option")[x].style.color;
     document.getElementById("status").style.color = color;
+    setSelectedOption({ status: e.target.value });
   };
+  //Update Status
+  const handleStatusUpdate = (id) => {
+    console.log(selectedOption);
+    fetch("http://localhost:5000/updateStatus?id=" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedOption),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const [allOrders, setAllOrders] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/getAllService")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllOrders(data);
+      });
+  }, []);
   return (
     <div className='row'>
       <Sidebar></Sidebar>
@@ -27,33 +54,54 @@ const ServiceList = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>Otto</td>
-                <td>
-                  <select
-                    id='status'
-                    onChange={handleStatusChange}
-                    style={{ color: "#FF4545" }}
-                  >
-                    <option
-                      value='pending'
-                      style={{ color: "#FF4545" }}
-                      selected='selected'
-                    >
-                      Pending
-                    </option>
-                    <option value='ongoing' style={{ color: "#FFBD3E" }}>
-                      Ongoing
-                    </option>
-                    <option value='complete' style={{ color: "#009444" }}>
-                      Complete
-                    </option>
-                  </select>
-                </td>
-              </tr>
+              {allOrders.map((order) => {
+                return (
+                  <tr>
+                    <th scope='row'>{order.name}</th>
+                    <td>{order.email}</td>
+                    <td>{order.project}</td>
+                    <td>{order.details}</td>
+                    <td>
+                      <form
+                        onSubmit={() => {
+                          handleStatusUpdate(order._id);
+                        }}
+                        action=''
+                        className='d-flex'
+                      >
+                        <select
+                          id='status'
+                          onChange={handleStatusChange}
+                          style={{ color: "#FF4545" }}
+                          value={order.status}
+                        >
+                          <option value='pending' style={{ color: "#FF4545" }}>
+                            Pending
+                          </option>
+                          <option value='ongoing' style={{ color: "#FFBD3E" }}>
+                            Ongoing
+                          </option>
+                          <option value='complete' style={{ color: "#009444" }}>
+                            Complete
+                          </option>
+                        </select>
+
+                        <button
+                          type='submit'
+                          className='btn-success'
+                          style={{
+                            height: "50%",
+                            borderRadius: "6px",
+                            fontSize: "small",
+                          }}
+                        >
+                          Update
+                        </button>
+                      </form>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
